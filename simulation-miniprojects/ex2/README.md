581e1a2776d7450a46993af8f4a378bd2e0330d0
artur-romao <arturomao@gmail.com>
2023-05-12 06:08:27 +0100

    se acabó

---
+++ simulation-miniprojects/ex2/README.md
@@ -0,0 +1,19 @@
+## Examples of usage
+
+### For program 1:
+```
+python3 euler_predator_prey.py
+
+python3 euler_predator_prey.py --x0 10 --y0 10 --a 0.1 --b 0.02 --d 0.02 --g 0.4 --dt 0.1 --tf 5000
+
+python3 euler_predator_prey.py --f params.txt
+```
+
+### For program 2:
+```
+python3 runge_kutta_predator_prey.py
+
+python3 runge_kutta_predator_prey.py --x0 10 --y0 10 --a 0.1 --b 0.02 --d 0.02 --g 0.4 --dt 0.1 --tf 5000
+
+python3 runge_kutta_predator_prey.py --f params.txt
+```
--- simulation-miniprojects/ex2/runge_kutta_predator_prey.py
+++ simulation-miniprojects/ex2/runge_kutta_predator_prey.py
@@ -2,7 +2,7 @@ import matplotlib.pyplot as plt
 import argparse
 
 def initialize(x0, y0, alpha, beta, delta, gamma, delta_t, tfinal):
-    global x, y, a, b, d, g, dt, tf, result_x, result_y
+    global x, y, a, b, d, g, dt, tf, xresult, yresult
     x        = x0
     y        = y0
     a        = alpha
@@ -11,38 +11,46 @@ def initialize(x0, y0, alpha, beta, delta, gamma, delta_t, tfinal):
     g        = gamma
     dt       = delta_t
     tf       = tfinal
-    result_x = [x]
-    result_y = [y]
+    xresult  = [x]
+    yresult  = [y]
 
 def observe():
-    global x, y, result_x, result_y
-    result_x.append(x)
-    result_y.append(y)
+    global x, y, xresult, yresult
+    xresult.append(x)
+    yresult.append(y)
 
 def update():
     global x, y, a, b, d, g, dt
     
-    k1_x = dt * (a * x - b * x * y)
-    k1_y = dt * (d * x * y - g * y)
+    k1_x = dt * f_x(x, y)
+    k1_y = dt * f_y(x, y)
 
-    k2_x = dt * (a * (x + dt / 2) - b * (x + dt / 2) * (y + k1_y / 2))
-    k2_y = dt * (d * (x + dt / 2) * (y + k1_y / 2) - g * (y + k1_y / 2))
+    k2_x = dt * f_x(x + dt / 2, y + k1_y / 2)
+    k2_y = dt * f_y(x + dt / 2, y + k1_y / 2)
 
-    k3_x = dt * (a * (x + dt / 2) - b * (x + dt / 2) * (y + k2_y / 2))
-    k3_y = dt * (d * (x + dt / 2) * (y + k2_y / 2) - g * (y + k2_y / 2))
+    k3_x = dt * f_x(x + dt / 2, y + k2_y / 2)
+    k3_y = dt * f_y(x + dt / 2, y + k2_y / 2)
 
-    k4_x = dt * (a * (x + dt) - b * (x + dt) * (y + k3_y))
-    k4_y = dt * (d * (x + dt) * (y + k3_y) - g * (y + k3_y))
+    k4_x = dt * f_x(x + dt, y + k3_y)
+    k4_y = dt * f_y(x + dt, y + k3_y)
 
     x    = x + (k1_x + 2 * k2_x + 2 * k3_x + k4_x) / 6
     y    = y + (k1_y + 2 * k2_y + 2 * k3_y + k4_y) / 6
 
 
+def f_x(x, y):
+    global a, b
+    return a * x - b * x * y
+
+def f_y(x, y):
+    global d, g
+    return d * x * y - g * y
+
 # Initialize the parser and parse the arguments from CLI
 parser = argparse.ArgumentParser()
 
-parser.add_argument("--x0", type=int, default=10, help="Parameter x0")
-parser.add_argument("--y0", type=int, default=10, help="Parameter y0")
+parser.add_argument("--x0", type=int, default=10, help="Initial population of prey")
+parser.add_argument("--y0", type=int, default=10, help="Initial population of predators")
 parser.add_argument("--a", type=float, default=0.1, help="Parameter alpha")
 parser.add_argument("--b", type=float, default=0.02, help="Parameter beta")
 parser.add_argument("--d", type=float, default=0.02, help="Parameter delta")
@@ -60,10 +68,10 @@ if params["f"]: # If the user specified a file, read the parameters from the fil
         with open(params["f"], "r") as f:
             params["x0"] = int(f.readline().split("=")[1])
             params["y0"] = int(f.readline().split("=")[1])
-            params["a"] = float(f.readline().split("=")[1])
-            params["b"] = float(f.readline().split("=")[1])
-            params["d"] = float(f.readline().split("=")[1])
-            params["g"] = float(f.readline().split("=")[1])
+            params["a"]  = float(f.readline().split("=")[1])
+            params["b"]  = float(f.readline().split("=")[1])
+            params["d"]  = float(f.readline().split("=")[1])
+            params["g"]  = float(f.readline().split("=")[1])
             params["dt"] = float(f.readline().split("=")[1])
             params["tf"] = int(f.readline().split("=")[1])
     except FileNotFoundError:
@@ -72,18 +80,25 @@ if params["f"]: # If the user specified a file, read the parameters from the fil
     f.close()
 
 # Initialize the system with the given parameters
-initialize(x0=params["x0"], y0=params["y0"], alpha=params["a"], beta=params["b"], delta=params["d"], gamma=params["g"], delta_t=params["dt"], tfinal=params["tf"])
+initialize(params["x0"], params["y0"], params["a"], params["b"], params["d"], params["g"], params["dt"], params["tf"])
 
 # Perform the simulation
 for t in range(tf):
     update()
     observe()
 
-# Plot the results
+# Plot the evolution of the populations over time
 plt.title("Predator-prey simulation using the Runge-Kutta method")
-plt.plot(result_x, label="Prey population (x)")
-plt.plot(result_y, label="Predator population (y)")
+plt.plot(xresult, label="Prey population (x)")
+plt.plot(yresult, label="Predator population (y)")
 plt.xlabel("Time")
 plt.ylabel("Population")
 plt.legend()
+plt.show()
+
+# Plot the evolution of the populations in the phase space
+plt.title("Predator-prey simulation using the Runge-Kutta method")
+plt.plot(xresult, yresult)
+plt.xlabel('Number of Preys (x)')
+plt.ylabel('Number of Predators (y)')
 plt.show()
